dea1e9e31bc3890a9ba8fbb53fa28e07892b4650
SabithHaneef <sabith.th.07@gmail.com>
2018-10-01 22:51:08 +0530

    added the basic layout of the calculator

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello There</Text>
-      </View>
-    );
-  }
-}
+import Button from './Button';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  top: {
+    paddingTop: 20,
+  },
+  bottom: {
+    flex: 1,
+  },
+  number: {
+    color: 'white',
+    backgroundColor: '#424242',
+    textAlign: 'right',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderColor: '#fff',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#fff',
   },
 });
+
+export const App = () => (
+  <View style={styles.container}>
+    <View style={styles.top}>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>1</Text>
+      <Text style={styles.number}>2</Text>
+    </View>
+    <View style={styles.bottom}>
+      <View style={styles.row}>
+        <Button text="C" />
+        <Button text="Pow" />
+        <Button text="Swap" />
+        <Button text="/" />
+      </View>
+      <View style={styles.row}>
+        <Button text="9" />
+        <Button text="8" />
+        <Button text="7" />
+        <Button text="X" />
+      </View>
+      <View style={styles.row}>
+        <Button text="6" />
+        <Button text="5" />
+        <Button text="4" />
+        <Button text="-" />
+      </View>
+      <View style={styles.row}>
+        <Button text="3" />
+        <Button text="2" />
+        <Button text="1" />
+        <Button text="+" />
+      </View>
+      <View style={styles.row}>
+        <Button text="0" />
+        <Button text="." />
+        <Button text="Enter" special />
+      </View>
+    </View>
+  </View>
+);
+
+export default App;
